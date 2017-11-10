9709acfd391a49b081f03688b5c205a3ee2795fe
Morgan Fudge <mfudge92@gmail.com>
2017-11-10 12:07:32 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -30,9 +30,7 @@ class Song
   end
 
   def self.find_by_name(name)
-    @@all.detect do |o|
-      o.name == name
-    end
+    @@all.detect { |o| o.name == name }
   end
 
   def self.find_or_create_by_name(name)
